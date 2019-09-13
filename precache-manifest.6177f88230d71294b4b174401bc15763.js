1562bae6b16884591a3b515a4cde62612bc0843d
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2019-09-13 17:25:57 +0530

    Updates

---
+++ precache-manifest.6177f88230d71294b4b174401bc15763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4563cacf990f4d1f890209bc19bf1f88",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d0c78027453fdcc4cde",
+    "url": "/monsters-rolodex/static/css/main.9342c29e.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "2d0c78027453fdcc4cde",
+    "url": "/monsters-rolodex/static/js/main.5b1d08ee.chunk.js"
+  },
+  {
+    "revision": "c7b84e328d40a43989ce",
+    "url": "/monsters-rolodex/static/js/runtime~main.45a8f4a3.js"
+  }
+]);
